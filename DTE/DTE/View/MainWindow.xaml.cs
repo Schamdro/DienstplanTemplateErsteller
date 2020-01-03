7d1dbcb653b2da16ae94cb3b284f4d9665d5cb39
Schamdro <39010488+Schamdro@users.noreply.github.com>
2020-01-03 20:02:30 +0100

    UI finished

--- DTE/DTE/View/MainWindow.xaml.cs
+++ DTE/DTE/View/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,6 +27,9 @@ namespace DTE.View
         {
             InitializeComponent();
 
+            yearText.Text = "" + (DateTime.Today.Month < 12 ? DateTime.Today.Year : DateTime.Today.Year + 1);
+            monthCombo.SelectedIndex = DateTime.Today.Month % 12;
+
             //ExcelInterface.SetTemplatePath(_templatePath);
             //ExcelInterface.OpenExcelApplication();
 
@@ -39,9 +43,53 @@ namespace DTE.View
             //ExcelInterface.MakeVisible();
         }
 
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (yearText == null) return;
+            TemplateResetter.SpecifyDate(monthCombo.SelectedIndex + 1, int.Parse(yearText.Text));
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void YearText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (yearText == null) return;
+            TemplateResetter.SpecifyDate(monthCombo.SelectedIndex + 1, int.Parse(yearText.Text));
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            ExcelInterface.SetTemplatePath(_templatePath);
+            ExcelInterface.OpenExcelApplication();
+            TemplateResetter.ResetEmployeeTableToWhite();
+            TemplateResetter.FillInWeekdays();
+            ExcelInterface.MakeVisible();
+        }
 
+        private void EmployeeNumberText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (employeeNumberText == null) return;
+            TemplateResetter.numberEmployees = int.Parse(employeeNumberText.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (employeeNumberText == null) return;
+            int employeeNumber = int.Parse(employeeNumberText.Text);
+            employeeNumber--;
+            employeeNumberText.Text = "" + employeeNumber;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (employeeNumberText == null) return;
+            int employeeNumber = int.Parse(employeeNumberText.Text);
+            employeeNumber++;
+            employeeNumberText.Text = "" + employeeNumber;
         }
     }
 }
